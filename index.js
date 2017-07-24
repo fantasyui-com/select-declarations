e8fe87b870c4f0b4030a9eee5a9fdb19607eb109
Captain Fantasy <captain@fantasyui.com>
2017-07-24 14:59:31 -0400

    Initial response must be string not null.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const postcss = require('postcss');
 module.exports = function(css, selector){
-  let response = null;
+  let response = "";
   postcss.parse(css).walkRules(function (rule) {
     if(rule.selector ===  selector){
         response += rule.nodes.map(i=>i.prop +': '+ i.value+';').join('\n')
